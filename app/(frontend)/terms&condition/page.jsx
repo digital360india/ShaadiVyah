2945edf7714cd341b64334687462430ab6c1d43e
sbadoni21 <97601162+sbadoni21@users.noreply.github.com>
2024-06-14 10:18:08 +0530

    new

--- app/(frontend)/terms&condition/page.jsx
+++ app/(frontend)/terms&condition/page.jsx
@@ -1,62 +1,94 @@
-'use client'
-import Popup from '@/components/Popup'
-import React from 'react'
-
+"use client";
+import Popup from "@/components/Popup";
+import React from "react";
 
 export default function page() {
   return (
     <div>
-    <div className='xl:px-[100px] xl:py-32 lg:py-20 py-10'>
-      <div className='md:px-20 px-6'>
-        <h1 className='font-semibold md:text-[48px] text-[32px] text-[#C9184A] text-center'> TERMS & CONDITIONS</h1>
-        <div className='text-[18px] mt-10'>
-        <p>Greetings from Shaadivyah. You accept and agree to be bound by the following terms and conditions by using our services</p>
-        
+      <div className="xl:px-[100px] xl:py-32 lg:py-20 py-10">
+        <div className="md:px-20 px-6">
+          <h1 className="font-semibold md:text-[48px] text-[32px] text-[#C9184A] text-center">
+            {" "}
+            TERMS & CONDITIONS
+          </h1>
+          <div className="text-[18px] mt-10">
+            <p>
+              Greetings from Shaadivyah. You accept and agree to be bound by the
+              following terms and conditions by using our services
+            </p>
+          </div>
+          <div>
+            <p className="text-[24px] font-semibold mt-10 ">
+              Service Agreement
+            </p>
+            <p className="text-[18px] mt-6">
+              {" "}
+              We offer consultation, coordination, and planning for weddings.
+              Every service&apos;s particulars will be outlined in a formal
+              agreement.
+            </p>
+            <div className="bg-black w-full h-[1px] mt-6"></div>
+          </div>
+          <div>
+            <p className="text-[24px] font-semibold mt-10 ">
+              {" "}
+              Terms of Payment{" "}
+            </p>
+            <p className="text-[18px] mt-6">
+              {" "}
+              The agreed-upon schedule for payments for services must be
+              followed. Extra fees will apply to any requested additional
+              services.
+            </p>
+            <div className="bg-black w-full h-[1px] mt-6"></div>
+          </div>
+          <div>
+            <p className="text-[24px] font-semibold mt-10 ">
+              {" "}
+              Policy for Cancellations
+            </p>
+            <p className="text-[18px] mt-6">
+              {" "}
+              Written cancellations are required. If applicable, refunds will be
+              handled in accordance with the conditions specified in the service
+              agreement.
+            </p>
+            <div className="bg-black w-full h-[1px] mt-6"></div>
+          </div>
+          <div>
+            <p className="text-[24px] font-semibold mt-10 "> Liability</p>
+            <p className="text-[18px] mt-6">
+              {" "}
+              Although we aim for perfection, Shaadivyah is not responsible for
+              any unanticipated incidents or situations that happen outside of
+              our control and could affect your wedding.
+            </p>
+            <div className="bg-black w-full h-[1px] mt-6"></div>
+          </div>
+          <div>
+            <p className="text-[24px] font-semibold mt-10 ">
+              {" "}
+              Intellectual Property
+            </p>
+            <p className="text-[18px] mt-6">
+              {" "}
+              Shaadivyah owns all of the text and image content on our website;
+              unauthorized use is prohibited.
+            </p>
+            <div className="bg-black w-full h-[1px] mt-6"></div>
+          </div>
+          <div>
+            <p className="text-[24px] font-semibold mt-10 "> Governing Law </p>
+            <p className="text-[18px] my-6">
+              {" "}
+              Indian law applies to these terms and conditions. Any
+              disagreements will be settled under the rules of our main office.
+            </p>
+          </div>
         </div>
-        <div>
-            <p className='text-[24px] font-semibold mt-10 '  >Service Agreement</p>
-            <p className='text-[18px] mt-6'>  We offer consultation, coordination, and planning for weddings. Every service's particulars will be outlined in a formal agreement.
-            </p>
-            <div className='bg-black w-full h-[1px] mt-6'></div>
-        </div>
-        <div>
-            <p className='text-[24px] font-semibold mt-10 '  > Terms of Payment </p>
-            <p className='text-[18px] mt-6'> The agreed-upon schedule for payments for services must be followed. Extra fees will apply to any requested additional services.
-            </p>
-            <div className='bg-black w-full h-[1px] mt-6'></div>
-        </div>
-         <div>
-            <p className='text-[24px] font-semibold mt-10 '  > Policy for Cancellations</p>
-            <p className='text-[18px] mt-6'>  Written cancellations are required. If applicable, refunds will be handled in accordance with the conditions specified in the service agreement.
-            </p>
-            <div className='bg-black w-full h-[1px] mt-6'></div>
-        </div>
-         <div>
-            <p className='text-[24px] font-semibold mt-10 '  > Liability</p>
-            <p className='text-[18px] mt-6'> Although we aim for perfection, Shaadivyah is not responsible for any unanticipated incidents or situations that happen outside of our control and could affect your wedding.
-
-            </p>
-            <div className='bg-black w-full h-[1px] mt-6'></div>
-        </div>
-         <div>
-            <p className='text-[24px] font-semibold mt-10 '  > Intellectual Property</p>
-            <p className='text-[18px] mt-6'> Shaadivyah owns all of the text and image content on our website; unauthorized use is prohibited.
-
-            </p>
-            <div className='bg-black w-full h-[1px] mt-6'></div>
-        </div> 
-        <div>
-            <p className='text-[24px] font-semibold mt-10 '  >  Governing Law </p>
-            <p className='text-[18px] my-6'> Indian law applies to these terms and conditions. Any disagreements will be settled under the rules of our main office.
-            </p>
-        </div> 
-        
-      
-        
       </div>
-    </div>
-    <div>
-    {/* <div className='lg:w-[1100px] lg:h-[636px] mx-4 p-5 rounded-xl border border-[#C9184A] flex justify-between '>
+      <div>
+        {/* <div className='lg:w-[1100px] lg:h-[636px] mx-4 p-5 rounded-xl border border-[#C9184A] flex justify-between '>
 
 <div className='hidden lg:block w-[515px] bg-black h-full rounded-l-xl'></div>
 <div className='mt-6 w-[432px]'>
@@ -81,8 +113,8 @@ export default function page() {
             </button></div>
             </div>
     </div> */}
-    <Popup/>
+        <Popup />
+      </div>
     </div>
-    </div>
-  )
+  );
 }
